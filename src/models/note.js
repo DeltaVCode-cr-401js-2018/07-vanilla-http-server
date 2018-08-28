b9fb4d8b8da41e0b80fbf7955ecea110f729628d
Keith Dahlby <keith@newbo.co>
2018-08-28 10:59:44 -0500

    Add some debug logging (don't forget to set DEBUG in .env!)

--- src/models/note.js
+++ src/models/note.js
@@ -1,22 +1,29 @@
 'use strict';
 
+const debug = require('debug')('models/note');
+
 import Storage from '../lib/storage';
 const noteStore = new Storage('notes');
 
 export default class Note {
   constructor(obj) {
     if (!obj) throw new Error('obj is required!');
+    debug('new Note', obj);
 
     this.title = obj.title;
     this.content = obj.content;
   }
 
   save() {
+    debug('save', this);
     return noteStore.save(this);
   }
 
   static fetchAll() {
-    return noteStore.getAll();
+    return noteStore.getAll().then(data => {
+      debug('fetchAll', data);
+      return data;
+    });
   }
 
   static findById(id) {
