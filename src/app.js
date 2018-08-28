84809a0287bad4fbfac9cba34ba39e8b7bdc17fb
Keith Dahlby <keith@newbo.co>
2018-08-28 10:43:46 -0500

    Install morgan

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 import express from 'express';
+import morgan from 'morgan';
+
 const app = express();
 
 // Make app available for testing
-export default app;
+module.exports = app;
 
 app.start = (port) =>
   new Promise((resolveCallback, rejectCallback) => {
@@ -28,11 +30,7 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
 // Log each request
-// TODO: learn about middleware!
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+app.use(morgan('dev'));
 
 app.post('/500', (req, res) => {
   throw new Error('Test Error');
