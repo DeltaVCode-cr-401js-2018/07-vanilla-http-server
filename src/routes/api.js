939234646df493625528d1682d79e10635ff4432
Keith Dahlby <keith@newbo.co>
2018-08-28 12:21:00 -0500

    Add modelFinder middleware (broken)

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,43 +5,39 @@ const router = express.Router();
 
 export default router;
 
-import Note from '../models/note';
-
 // Get all notes
-router.get('/api/notes', (req, res) => {
-  Note.find({})
-    .then(notes => {
-      res.json(notes);
+router.get('/api/:model', (req, res, next) => {
+  req.Model.find({})
+    .then(models => {
+      res.json(models);
     });
 });
 
 // Create a note
-router.post('/api/notes', (req, res) => {
+router.post('/api/:model', (req, res) => {
   if (!req.body || !req.body.title) {
     res.send(400);
     res.end();
     return;
   }
 
-  var newNote = new Note({
-    ...req.body,
-    created: new Date(),
-  });
-  newNote.save()
+  var newModel = new req.Model(req.body);
+  newModel.save()
     .then(saved => {
       res.json(saved);
     });
 });
 
 // Get an individual note
-router.get('/api/notes/:id', (req, res) => {
-  return Note.findById(req.params.id)
-    .then(note => {
-      res.json(note);
+router.get('/api/:model/:id', (req, res) => {
+  return req.Model.findById(req.params.id)
+    .then(model => {
+      res.json(model);
     });
 });
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/:model/:id', (req, res) => {
+  // TODO: implement delete?
   res.json({
     message: `ID ${req.params.id} was deleted`,
   });
